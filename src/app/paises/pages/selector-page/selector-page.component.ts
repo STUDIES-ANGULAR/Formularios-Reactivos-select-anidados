e5ca78e80f44f088e07992a2ea72e18479d68c0b
Arley Rivas Granado <arrivasg@unipacifico.edu.co>
2022-11-01 23:58:54 -0500

    Con RXJS limpiar select pais cuando el select continente cambia

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { switchMap, tap } from 'rxjs/operators'
+
 import { PaisesService } from '../../services/paises.service';
 
 @Component({
@@ -24,16 +27,32 @@ export class SelectorPageComponent implements OnInit {
   ngOnInit(): void {
     this.continentes = this.paisesService.continentes;
 
-    this.miFormulario.get('continente')?.valueChanges
-        .subscribe( continente => {
-          console.log(continente);
+    //
+    // this.miFormulario.get('continente')?.valueChanges
+    //     .subscribe( continente => {
+    //       console.log(continente);
 
-          this.paisesService.getPaisesPorContinenete( continente )
-              .subscribe( paises => {
-                this.paises = paises;
-                console.log(this.paises)
-              })
-        })
+    //       this.paisesService.getPaisesPorContinenete( continente )
+    //           .subscribe( paises => {
+    //             this.paises = paises;
+    //             console.log(this.paises)
+    //           })
+    //     })
+
+
+    this.miFormulario.get('continente')?.valueChanges
+      .pipe(
+        //con el tap me permite recibir el continente, como no me interesa lo pongo as _  (el _ es un estandar ) y al saber que cambia, reseteo el valor del pais
+        tap( ( _ ) =>{
+          this.miFormulario.get('pais')?.reset('');
+        }),
+        //con el switchMap capturamos el observable y devolvemos un nuevo observable (modificamos la salida del observable original a paises)
+        switchMap( continente => this.paisesService.getPaisesPorContinenete( continente) )
+      )
+      .subscribe( paises =>{
+        console.log(paises);
+        this.paises = paises
+      })
   }
 
   guardar(){
