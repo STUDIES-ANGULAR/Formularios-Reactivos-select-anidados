2dc5b345b6fce680ebbe1c4fa2eab2c94dc45376
Arley Rivas Granado <arrivasg@unipacifico.edu.co>
2022-11-02 22:44:03 -0500

    cambio endpoint y agregar interface a paises por continente

--- src/app/paises/services/paises.service.ts
+++ src/app/paises/services/paises.service.ts
@@ -1,15 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
-import { Pais } from '../interfaces/paises.interfaces';
+import { Pais, PaisSmall } from '../interfaces/paises.interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PaisesService {
 
-  private baseUrl: string = 'https://restcountries.com/v3.1'
-  private baseUrlv2: string = 'https://restcountries.com/v2'
+  private baseUrl: string = 'https://restcountries.com/v2';
   private _continentes: string[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
 
   get continentes(): string[] {
@@ -19,8 +18,11 @@ export class PaisesService {
   constructor( private http: HttpClient ) { }
   
   
-  getPaisesPorContinenete(region: string){
-    return this.http.get(`${ this.baseUrl }/region/${ region }?fields=name,cca3`);
+  getPaisesPorContinenete( region: string ): Observable<PaisSmall[]>{
+    
+    const url =`${ this.baseUrl }/region/${ region }?fields=alpha3Code,name`;
+    return this.http.get<PaisSmall[]>( url );
+
   }
 
   getPaisPorCodigo( codigo: string): Observable<Pais | null> {
@@ -29,8 +31,10 @@ export class PaisesService {
       return of( null )
     }
 
-    const url = ` ${ this.baseUrlv2 }/alpha/${ codigo }`;
+    const url = ` ${ this.baseUrl }/alpha/${ codigo }`;
     return this.http.get<Pais>(url);
 
   }
+
+
 }
